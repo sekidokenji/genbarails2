c31a77e8521264b776d3da7cf342712da89e252a
sekidokenji <skdknt2018@gmail.com>
2022-06-18 18:02:04 +0900

    3-3-2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,6 @@
 class TasksController < ApplicationController
   def index
+    @tasks = Task.all
   end
 
   def show
@@ -11,4 +12,16 @@ class TasksController < ApplicationController
 
   def edit
   end
+
+  def create
+    task = Task.new(task_params)
+    task.save!
+    redirect_to tasks_url, notice: "タスク「#{task.name}」 を登録しました。"
+  end
+  
+
+  def task_params
+    params.require(:task).permit(:name, :description)  
+  end
+  
 end
