adcb2c2ef7cd177c968a68ab07bf83f1550129ec
sekidokenji <skdknt2018@gmail.com>
2022-06-28 12:17:25 +0900

    add: admin新規登録画面

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_041857) do
+ActiveRecord::Schema.define(version: 2022_06_23_220933) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
