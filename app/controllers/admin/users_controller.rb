4a67c76af7d28fbcae35c81035075a1931370aa1
sekidokenji <skdknt2018@gmail.com>
2022-07-08 01:13:51 +0900

    add: ログイン機能

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -14,18 +14,40 @@ class Admin::UsersController < ApplicationController
   end
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def index
+    @users = User.all
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(user_params)
+      redirect_to admin_user_url(@user), notice: "ユーザー　「#{@user.name}」を登録しました。"
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @user =User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_url, notice: "ユーザー　「#{@user.name}」　を削除しました。"
   end
 
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :admin, :password, :password_confirmation)
+  end
+
+  def require_admin
+    redirect_to root_url unless current_user.admin?
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_23_220933) do
+ActiveRecord::Schema.define(version: 2022_07_03_222422) do
 
   create_table "tasks", force: :cascade do |t|
     t.string "name", null: false
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -25,6 +27,7 @@ ActiveRecord::Schema.define(version: 2022_06_23_220933) do
     t.string "password_digest", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "admin", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
